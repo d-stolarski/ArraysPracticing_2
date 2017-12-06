735db3effc1a4c1d0b1030f8d8107583ef5328dd
Daniel Stolarski <d.stolarski@o2.pl>
2017-12-06 09:39:19 +0100

    final commit

---
+++ src/Zad02.java
@@ -0,0 +1,10 @@
+public class Zad02 {
+    public static void main(String[] args) {
+
+        int[] values = {2, 4, 6, 9, 12};
+
+        int sum = values[0] + values[2] + values[4];
+
+        System.out.printf("%d + %d + %d = %d ", values[0], values[2], values[4], sum);
+    }
+}
